d58ab8a54638e494b800390dcac8819058ad803e
chewzzz <chewziqing@gmail.com>
2023-03-02 13:24:29 +0800

    Add rawData.txt; modify how options are stored in json

--- backend/utils/rewriteScores.js
+++ backend/utils/rewriteScores.js
@@ -1,4 +1,31 @@
-const { axios } = require('axios')
+const axios = require('axios')
 const { BASE_URL } = require('./config')
 
-console.log(BASE_URL)
+async function rewriteScores() {
+    try {
+        const result = await axios.get(BASE_URL)
+        const allQuestions = result.data
+
+        const updatedAllQuestions = allQuestions.map((ele, idx) => {
+            const updatedGood = ele.good.map(g => {
+                return {
+                    ...g,
+                    score: 0
+                }
+            })
+            return {
+                ...ele,
+                good: updatedGood
+            }
+        })
+        console.log(updatedAllQuestions)
+        await axios.patch(BASE_URL, {
+            ...updatedAllQuestions,
+        }).then(() => console.log('Scores cleared!'))
+
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+rewriteScores()
