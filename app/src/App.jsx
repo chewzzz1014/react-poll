ae04f98d496e4c2c73c3a25190a5e0aad450318d
chewzzz <chewziqing@gmail.com>
2023-03-02 10:54:00 +0800

    Add json-server as backend

--- app/src/App.jsx
+++ app/src/App.jsx
@@ -2,34 +2,53 @@ import { useState, useEffect } from 'react'
 
 import './App.css'
 import Question from './components/Question'
-import questionData from './data/questions.json'
+import axios from 'axios'
 
 function App() {
-  const [data, setData] = useState({
-    idx: 0,
-    score: 0
-  })
-  console.log(`Current Score: ${data.score}`)
+  const [data, setData] = useState([])
+  const [currentIdx, setCurrentIdx] = useState(0)
 
-  const questionEle = questionData.map((ele, idx) =>
+  async function fetchData() {
+    const d = await axios.get('http://localhost:3000/questions')
+    setData(d.data)
+  }
+
+  useEffect(() => {
+    fetchData()
+  }, [])
+
+  const questionEle = data && data.map((ele, idx) =>
     <Question
       key={idx}
       question={ele}
       handleBtnClick={handleBtnClick}
+      questionId={idx}
     />
   )
 
-  function handleBtnClick(e, isBad) {
-    const tmp = {
-      idx: data.idx + 1,
-      score: !isBad ? data.score + 1 : data.score
+  async function handleBtnClick(e, questionId, isBad) {
+    e.preventDefault()
+
+    try {
+      if (!isBad) {
+        const url = `http://localhost:3000/questions/${questionId}`
+        const result = await axios.get(url)
+        const oldScore = result.data.score || 0
+
+        await axios.patch(url, {
+          score: oldScore + 1
+        }).then(() => console.log('Score updated!'))
+      }
+    } catch (e) {
+      console.log(e)
     }
-    setData(tmp)
+
+    setCurrentIdx(currentIdx + 1)
   }
 
   return (
     <div className='w-screen h-screen flex justify-center items-center'>
-      {questionEle[data.idx] ||
+      {questionEle[currentIdx] ||
         <h1 className='font-bold text-3xl'>Thank You!</h1>
       }
     </div>
--- app/src/components/Question.jsx
+++ app/src/components/Question.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import Option from './Option'
 import shuffle from '../utils/shuffleArr'
 
-export default function Question({ question, handleBtnClick }) {
+export default function Question({ question, handleBtnClick, questionId }) {
     const badOption = question.bad
     // shuffle array
     const options = shuffle([...question.good, question.bad])
@@ -11,7 +11,14 @@ export default function Question({ question, handleBtnClick }) {
         <div className='p-3'>
             <h1 className='mb-12 text-4xl text-center'><b>{question['A/B Testing']}</b></h1>
             <div className='options flex'>
-                {options.slice(0, 2).map((o, idx) => <Option key={idx} text={o} isBad={o === badOption} handleBtnClick={handleBtnClick} />)}
+                {options.slice(0, 2).map((o, idx) => <Option
+                    key={idx}
+                    text={o}
+                    isBad={o === badOption}
+                    handleBtnClick={handleBtnClick}
+                    questionId={questionId}
+                />
+                )}
             </div>
         </div>
     )
