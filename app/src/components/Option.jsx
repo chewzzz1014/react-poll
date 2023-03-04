8358e06a05b714053c498681e0bb442931bb264b
chewzzz <chewziqing@gmail.com>
2023-03-04 17:25:36 +0800

    Remove red text for bad option

--- app/src/components/Option.jsx
+++ app/src/components/Option.jsx
@@ -3,7 +3,8 @@ import React from 'react'
 export default function Option({ text, isBad, handleBtnClick, questionId, optionId }) {
     return (
         <div className='flex-1 mt-5 option'>
-            <p className={`text-2xl ${isBad ? 'text-red-600' : ''}`}>{text}</p>
+            {/* ${isBad ? 'text-red-600' : ''} */}
+            <p className={`text-2xl`}>{text}</p>
             <button
                 className='rounded-md p-4 px-8 mt-5 bg-gray-900 text-white font-bold text-xl self-center '
                 onClick={(e) => handleBtnClick(e, questionId, optionId, isBad)}
